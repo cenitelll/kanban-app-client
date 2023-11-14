52c5e231f6ea120de50450facbb18d4da56c7fd0
Theconnoiser <cenitelll@gmail.com>
2023-11-14 21:45:20 +0200

    Made the navigation for the boards, connected the creation of the board to the main button, connected the dnd for the Sidebar. (changes were only in Sidebar.jsx, Home.jsx)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,29 @@
 import { Box } from "@mui/material";
 import LoadingButton from "@mui/lab/LoadingButton";
+import { useDispatch } from "react-redux";
+import { setBoards } from "../redux/features/boardSlice";
+import { useNavigate } from "react-router-dom";
+import boardApi from "../api/boardApi";
+import { useState } from "react";
 
 const Home = () => {
-    const createBoard = () => {};
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [loading, setLoading] = useState(false);
+
+    const createBoard = async () => {
+        setLoading(true);
+        try {
+            const res = await boardApi.create();
+            dispatch(setBoards([res]));
+            navigate(`/boards/${res.id}`);
+        } catch (err) {
+            alert(err);
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
         <Box
             sx={{
@@ -16,6 +37,7 @@ const Home = () => {
                 variant="outlined"
                 color="success"
                 onClick={createBoard}
+                loading={loading}
             >
                 Click here to create your first board
             </LoadingButton>
